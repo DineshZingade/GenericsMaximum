08f1575408e80ed3c01fe7790a1df5e7e4c83c9e
DineshZingade <zingadedinesh1991@gmail.com>
2022-11-23 08:34:04 +0530

    Added:Find Float Maximum

--- src/com/bridgelabz/GenericsMaximum.java
+++ src/com/bridgelabz/GenericsMaximum.java
@@ -1,6 +1,6 @@
 package com.bridgelabz;
 
-public class GenericsMaximumInt {
+public class GenericsMaximum {
 
 	public static int maximum(Integer a1, Integer a2, Integer a3) {
 		Integer max = a1;
@@ -11,7 +11,19 @@ public class GenericsMaximumInt {
 		return max;
 	}
 
+	public static float floatMaximum(Float a1, Float a2, Float a3) {
+		Float max = a1;
+		if (a2.compareTo(max) > 0)
+			max = a2;
+		if (a3.compareTo(max) > 0)
+			max = a3;
+		return max;
+	}
+
 	public static void main(String[] args) {
 		System.out.println(maximum(2, 6, 8));
+		System.out.println(floatMaximum(2.5f, 6.8f, 8.8f));
+
 	}
+
 }
